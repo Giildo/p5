ee4c7dc2c0b6581075a897a15e553ef79c7c7135
Giildo <giildo.jm@gmail.com>
2018-03-30 14:04:13 +0200

    Modification du JS pour prendre en charge la zone morte sous le footer

--- public/js/footerPosition.js
+++ public/js/footerPosition.js
@@ -1,5 +1,7 @@
 $(function () {
-    if ($('body').height() < $(window).height()) {
+    var body = $('body').height() - 220 + 66;
+    var windows = $(window).height() - 220;
+    if (body < windows) {
         $('footer').css('position', 'fixed')
             .css('bottom', '0')
             .css('width', '100%');
